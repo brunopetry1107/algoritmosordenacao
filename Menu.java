f5ae5cd911e919afaac1bd877639b0ee7e3d1c87
Bruno Petry <brunopetry1107@gmail.com>
2021-12-06 21:42:52 -0300

    Initial commit

---
+++ Menu.java
@@ -0,0 +1,36 @@
+package ed2.projeto.algoritmosordenacao;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        Scanner escolha = new Scanner(System.in);
+        System.out.println("Digite o número da opção desejada: ");
+        System.out.println("(1) Buscar aluno");
+        System.out.println("(2) Remover aluno");
+        System.out.println("(3) Exibir tabela hash");
+        System.out.println("(4) Sair");
+
+
+        switch (escolha);
+        {
+            case 1:
+        }
+
+        {
+            case 2:
+        }
+
+        {
+            case 3:
+        }
+
+        {
+            case 4:
+        }
+
+
+        }
+
+}
